2349f3a1e4f7254e7617a69d3dcb68823aedab31
overjoy1008 <overjoy1008@gmail.com>
2025-12-03 13:52:59 +0900

    라이브 모드 + 테스트 모드 추가

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
@@ -14,17 +15,21 @@ app.use(cors({
 
 // TODO: 개발자센터에 로그인해서 내 결제위젯 연동 키 > 시크릿 키를 입력하세요. 시크릿 키는 외부에 공개되면 안돼요.
 // @docs https://docs.tosspayments.com/reference/using-api/api-keys
-const secretKey = "live_gsk_eqRGgYO1r5AklapJPw4orQnN2Eya";
-// const secretKey = "test_sk_26DlbXAaV0aPRGYOB9p5rqY50Q9R";
+const secretKey = process.env.TOSS_SECRET_KEY;
+const secretTestKey = process.env.TOSS_SECRET_TEST_KEY;
 
 // 토스페이먼츠 API는 시크릿 키를 사용자 ID로 사용하고, 비밀번호는 사용하지 않습니다.
 // 비밀번호가 없다는 것을 알리기 위해 시크릿 키 뒤에 콜론을 추가합니다.
 // @docs https://docs.tosspayments.com/reference/using-api/authorization#%EC%9D%B8%EC%A6%9D
 const encryptedSecretKey = "Basic " + Buffer.from(secretKey + ":").toString("base64");
+const encryptedSecretTestKey = "Basic " + Buffer.from(secretTestKey + ":").toString("base64");
 
 // 결제 승인
 app.post("/confirm", function (req, res) {
   const { paymentKey, orderId, amount } = req.body;
+
+  const isTestMode = req.body.testMode === true || paymentKey.startsWith("test_");
+  const keyToUse = isTestMode ? encryptedSecretTestKey : encryptedSecretKey;
 
   // 결제 승인 API를 호출하세요.
   // 결제를 승인하면 결제수단에서 금액이 차감돼요.
@@ -32,7 +37,7 @@ app.post("/confirm", function (req, res) {
   fetch("https://api.tosspayments.com/v1/payments/confirm", {
     method: "POST",
     headers: {
-      Authorization: encryptedSecretKey,
+      Authorization: keyToUse,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
